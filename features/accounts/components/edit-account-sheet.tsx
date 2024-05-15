8ca14e679b8a79ddb0dea24ea0dd335d9ac63380
awais512 <raza.awais25@gmail.com>
2024-05-15 23:25:50 +0100

    Update account

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -8,10 +8,10 @@ import {
 import { AccountForm } from "./account-form";
 import { insertAccountSchema } from "@/db/schema";
 import { z } from "zod";
-import { useCreateAccount } from "@/features/accounts/api/use-create-account";
 import { useOpenAccount } from "../hooks/use-open-account";
 import { useGetAccount } from "../api/use-get-account";
 import { Loader2 } from "lucide-react";
+import { useEditAccount } from "../api/use-edit-account";
 
 const formSchema = insertAccountSchema.pick({ name: true });
 type FormValues = z.input<typeof formSchema>;
@@ -20,12 +20,13 @@ export const EditAccountSheet = () => {
   const { isOpen, onClose, id } = useOpenAccount();
 
   const accountQuery = useGetAccount(id);
-  const mutation = useCreateAccount();
+  const editMutation = useEditAccount(id);
 
+  const isPending = editMutation.isPending;
   const isLoading = accountQuery.isLoading;
 
   const onSubmit = (values: FormValues) => {
-    mutation.mutate(values, { onSuccess: () => onClose() });
+    editMutation.mutate(values, { onSuccess: () => onClose() });
   };
 
   const defaultValues = accountQuery.data
@@ -49,7 +50,7 @@ export const EditAccountSheet = () => {
           <AccountForm
             id={id}
             onSubmit={onSubmit}
-            disabled={mutation.isPending}
+            disabled={isPending}
             defaultValues={defaultValues}
           />
         )}
